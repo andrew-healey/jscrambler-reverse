4a5e0c317c39c814760234f1de3385e8777892a2
Andrew Healey <doolie.healey@gmail.com>
2022-06-07 02:51:36 -0400

    Finally working dynamic (changing code, size) viz.

--- src/transformations/control-flow.js
+++ src/transformations/control-flow.js
@@ -1,13 +1,23 @@
-import assert from "node:assert";
-import { is } from "../assert-utils.js";
-import { codeGen } from "shift-codegen";
-import {refactor} from "shift-refactor";
-import { writeFileSync } from "node:fs";
-import Shift from "shift-ast";
-import Scarbon from "scarbon";
+import assert from 'node:assert';
+import { is } from '../assert-utils.js';
+import { codeGen } from 'shift-codegen';
+import { refactor } from 'shift-refactor';
+import { writeFileSync } from 'node:fs';
+import Shift from 'shift-ast';
+import Scarbon from 'scarbon';
 
-const countParents = ({id}, cases) => {
+const countParents = ({ id }, cases) => {
   return cases.filter((aCase) => aCase.children.includes(id)).length;
+};
+
+const deleteNodes = (cases, idsToKill) => {
+  idsToKill.forEach((id) => {
+    const idxToKill = cases.findIndex((node) => node.id == id);
+    if (idxToKill !== -1) {
+      cases.splice(idxToKill, 1);
+    }
+  });
+  return idsToKill;
 };
 
 const replaceChildren = (cases, idToKill, newId) => {
@@ -22,38 +32,40 @@ const replaceChildren = (cases, idToKill, newId) => {
   return newEdgeLog;
 };
 
-const getCase = (id, cases) => cases.filter((aCase) => aCase.id === id);
+const getCase = (id, cases) => cases.find((aCase) => aCase.id === id);
 
 const checkCase = (aCase, endVal) => {
-  assert.notEqual(aCase.id,undefined);
+  assert.notEqual(aCase.id, undefined);
   assert(aCase.children.length < 3);
   assert.equal(
     !!aCase.conditional,
     aCase.children.length == 2,
     "Unconditional nodes can't point to two children." +
       aCase.conditional +
-      aCase.children.length
+      aCase.children.length,
   );
 };
 
-const renderer=await new Scarbon({
-    lang:"js"
+const renderer = await new Scarbon({
+  lang: 'js',
 });
-const scarbon = (code) =>{
-    const svg=renderer.svg(code);
-    const dataUrl=`data:image/svg+xml;utf8,${svg.replace(/$\s/gm," ")}`;
-    return svg;
+const scarbon = (code) => {
+  const svg = renderer.svg(code);
+  const dataUrl = `data:image/svg+xml;utf8,${svg.replace(/$\s/gm, ' ')}`;
+  return svg;
 };
 
-console.log(scarbon(`
+console.log(
+  scarbon(`
 console.log(a+2*3);
-`));
+`),
+);
 
 const stringify = (statements) => {
   const newBlock = new Shift.Block({
     statements,
   });
-  const sess=refactor(newBlock);
+  const sess = refactor(newBlock);
   return sess.codegen()[0];
   //return codeGen(newBlock);
 };
@@ -66,20 +78,20 @@ const stringify = (statements) => {
 const makeCase = (shiftCase, stateName) => {
   const { test, consequent } = shiftCase;
 
-  is(test, "LiteralNumericExpression");
+  is(test, 'LiteralNumericExpression');
 
   const id = test.value;
 
   assert.notEqual(consequent.length, 0);
 
-  is(consequent[consequent.length - 1], "BreakStatement");
+  is(consequent[consequent.length - 1], 'BreakStatement');
 
   const statements = consequent.slice(0, consequent.length - 1);
 
   assert.notEqual(statements.length, 0);
 
   const lastStatement = statements[statements.length - 1];
-  if (lastStatement.type === "ReturnStatement") {
+  if (lastStatement.type === 'ReturnStatement') {
     return {
       id,
       statements,
@@ -87,22 +99,22 @@ const makeCase = (shiftCase, stateName) => {
       conditional: undefined,
     };
   }
-  if (lastStatement.type === "ReturnStatement") {
+  if (lastStatement.type === 'ReturnStatement') {
     console.log(statements.map((st) => st.type));
   }
 
-  is(lastStatement, "ExpressionStatement");
+  is(lastStatement, 'ExpressionStatement');
   const assignExp = lastStatement.expression;
-  is(assignExp, "AssignmentExpression");
+  is(assignExp, 'AssignmentExpression');
 
   const { binding, expression } = assignExp;
 
-  is(binding, "AssignmentTargetIdentifier");
+  is(binding, 'AssignmentTargetIdentifier');
   assert.equal(binding.name, stateName);
 
   const prevStatements = statements.slice(statements.length - 1);
 
-  if (expression.type === "LiteralNumericExpression") {
+  if (expression.type === 'LiteralNumericExpression') {
     // Simple linear step.
     return {
       id,
@@ -112,12 +124,12 @@ const makeCase = (shiftCase, stateName) => {
     };
   }
 
-  if (expression.type === "ConditionalExpression") {
+  if (expression.type === 'ConditionalExpression') {
     const conditional = expression.test;
 
     const { consequent, alternate } = expression;
-    is(consequent, "LiteralNumericExpression");
-    is(alternate, "LiteralNumericExpression");
+    is(consequent, 'LiteralNumericExpression');
+    is(alternate, 'LiteralNumericExpression');
 
     return {
       id,
@@ -134,6 +146,7 @@ const makeCase = (shiftCase, stateName) => {
  * type Reduction {
  *   nodesDeleted:number[]
  *   edgesAdded:[number,number][]
+ *   editedNodes:number[]
  * }
  */
 
@@ -150,27 +163,63 @@ const reduceCases = (cases, stateName, startVal) => {
   for (let caseNum in cases) {
     const aCase = cases[caseNum];
 
-    // Check for disconnected cases.
-    if (startVal !== aCase.id && countParents(aCase,cases) == 0) {
-        console.log("Popping");
-        cases.splice(caseNum,1);
-        return {
-            nodesDeleted:[aCase.id],
-            edgesAdded:[],
-        };
+    // Check for floating cases.
+    if (startVal !== aCase.id && countParents(aCase, cases) == 0) {
+      cases.splice(caseNum, 1);
+      return {
+        nodesDeleted: [aCase.id],
+        edgesAdded: [],
+        editedNodes: [],
+      };
     }
 
     // Check for linearness.
     if (aCase.children.length == 1) {
-      const child = getCase(aCase.children[0],cases);
-      if (child !== aCase && countParents(child,cases) == 1) {
-        child.statements = [...aCase.statements, ...child.statements];
-        cases.splice(caseNum,1); // Remove dead case.
-        const newEdges = replaceChildren(cases, aCase.id, child.id);
+      const child = getCase(aCase.children[0], cases);
+      if (child !== aCase && countParents(child, cases) == 1) {
+        aCase.statements = [...aCase.statements, ...child.statements];
+        aCase.condition = child.condition;
+        aCase.children = child.children;
+        const nodesDeleted = deleteNodes(cases, [child.id]);
+        const newEdges = replaceChildren(cases, child.id, aCase.id);
 
         return {
-          nodesDeleted: [aCase.id],
+          nodesDeleted,
           edgesAdded: newEdges,
+          editedNodes: [aCase],
+        };
+      }
+    }
+
+    if (aCase.children.length == 2) {
+      const cons = getCase(aCase.children[0], cases);
+      const alt = getCase(aCase.children[1], cases);
+      console.log('Found two children. IDS are', cons.id, alt.id);
+
+      const isSimpleLeaf = (node) =>
+        countParents(node, cases) == 1 && node.children.length == 0;
+
+      if (isSimpleLeaf(cons) && isSimpleLeaf(alt)) {
+        const makeBlock = (node) =>
+          new Shift.BlockStatement({
+            block: new Shift.Block({
+              statements: node.statements,
+            }),
+          });
+        const finalIf = new Shift.IfStatement({
+          test: aCase.conditional,
+          consequent: makeBlock(cons),
+          alternate: makeBlock(alt),
+        });
+        const nodesDeleted = deleteNodes(cases, [cons.id, alt.id]);
+        aCase.statements = [...aCase.statements, finalIf];
+        aCase.children = [];
+        aCase.conditional = undefined;
+
+        return {
+          nodesDeleted,
+          edgesAdded: [],
+          editedNodes: [aCase],
         };
       }
     }
@@ -181,24 +230,24 @@ const reduceCases = (cases, stateName, startVal) => {
 
 const deepenFlow = (sess) => {
   const switcher = sess(
-    `VariableDeclarationStatement:first-child + ForStatement > BlockStatement > Block > SwitchStatement`
+    `VariableDeclarationStatement:first-child + ForStatement > BlockStatement > Block > SwitchStatement`,
   ).get(0);
-  is(switcher, "SwitchStatement");
+  is(switcher, 'SwitchStatement');
 
   if (!switcher) return false;
 
   const forLoop = sess(switcher).parents().parents().parents().get(0);
-  is(forLoop, "ForStatement");
+  is(forLoop, 'ForStatement');
 
   const containingBlock = sess(forLoop).parents().get(0); // Block-like object.
   assert(
     containingBlock.statements,
-    "Containing block has no statements. Not blocky. Type: " +
-      containingBlock?.type
+    'Containing block has no statements. Not blocky. Type: ' +
+      containingBlock?.type,
   );
 
   const stateDeclSt = sess(containingBlock).statements().get(0);
-  is(stateDeclSt, "VariableDeclarationStatement");
+  is(stateDeclSt, 'VariableDeclarationStatement');
 
   const decl = stateDeclSt.declaration;
   assert.equal(decl.declarators.length, 1);
@@ -218,19 +267,19 @@ const deepenFlow = (sess) => {
 
   const { test } = forLoop;
 
-  is(test, "BinaryExpression");
+  is(test, 'BinaryExpression');
 
   const { left, right } = test;
 
-  is(left, "IdentifierExpression");
+  is(left, 'IdentifierExpression');
   assert.equal(left.name, stateName);
 
-  is(right, "LiteralNumericExpression");
+  is(right, 'LiteralNumericExpression');
 
   const endVal = right.value;
 
   const { discriminant, cases } = switcher;
-  is(discriminant, "IdentifierExpression");
+  is(discriminant, 'IdentifierExpression');
   assert.equal(discriminant.name, stateName);
 
   /*
@@ -249,24 +298,26 @@ const deepenFlow = (sess) => {
   };
 
   const foundCases = cases.map((foundCase) =>
-    makeCase(foundCase, stateName, startVal)
+    makeCase(foundCase, stateName, startVal),
   );
 
   const allCases = [...foundCases, builtInCase];
 
   // For logging purposes
-  const bareCases = allCases.map((aCase) => ({
-    id: aCase.id,
-    code: stringify(aCase.statements),
-  })).map(aCase=>{
-      const {code} = aCase;
+  const bareCases = allCases
+    .map((aCase) => ({
+      id: aCase.id,
+      code: stringify(aCase.statements),
+    }))
+    .map((aCase) => {
+      const { code } = aCase;
       return {
-          ...aCase,
-          svg:scarbon(code)
-      }
-  });
+        ...aCase,
+        svg: scarbon(code),
+      };
+    });
   const bareEdges = allCases.flatMap(({ id, children }) =>
-    children.map((child) => [id, child])
+    children.map((child) => [id, child]),
   );
   const allReplacements = [];
 
@@ -280,7 +331,7 @@ const deepenFlow = (sess) => {
 
     const serialized = JSON.stringify(saveObj, null, 2);
 
-    writeFileSync("graph.json", serialized);
+    writeFileSync('graph.json', serialized);
   };
 
   save();
@@ -292,12 +343,30 @@ const deepenFlow = (sess) => {
       break;
     }
 
-    allReplacements.push(replacement);
+    const newEdits = replacement.editedNodes.map((node) => {
+      const code = stringify(node.statements);
+      const svg = scarbon(code);
+
+      return {
+        id: node.id,
+        code,
+        svg,
+      };
+    });
+
+    const newReplacement = {
+      nodesDeleted: replacement.nodesDeleted,
+      edgesAdded: replacement.edgesAdded,
+      editedNodes: newEdits,
+    };
+
+    allReplacements.push(newReplacement);
 
     save();
   }
 
   assert.equal(allCases.length, 1, "The graph didn't fully reduce.");
+  assert.fail('Demo fail.');
 
   const [finalCase] = allCases;
   assert.equal(finalCase.children.length, 0);
@@ -314,7 +383,6 @@ const deepenFlow = (sess) => {
 };
 
 export default (sess) => {
-
   // Cleanup.
   const { session } = sess;
 
--- viz.js
+++ viz.js
@@ -1,18 +1,22 @@
 window.onload = async () => {
-  const { cases, edges, steps } = await (await fetch("graph.json")).json();
+  const { cases, edges, steps } = await (await fetch('graph.json')).json();
 
   let currStep = 0;
 
-  const svg = d3.select("svg");
+  const svg = d3.select('svg');
 
   const width = 1920;
   const height = 900;
 
   const rad = 50;
-  const dist = rad * 3;
-  const charge = rad * -10;
+  const linesRatio = 0.2;
+  const dist = (rad + 20) * 3;
+  const charge = (rad + 10) * -10;
 
   let nodesArray, linksArray;
+
+  const getRadius = (node) =>
+    rad + Math.floor(node.code.split('\n').length * linesRatio);
 
   const makeArrays = () => {
     nodesArray = cases.map((oneCase) => ({
@@ -30,80 +34,110 @@ window.onload = async () => {
 
   makeArrays();
 
-  svg.append("defs").html(`
-<defs>
+  const markerBoxWidth = 10;
+  const markerBoxHeight = 10;
+  const refX = markerBoxWidth / 2 + rad / 2;
+  const refY = markerBoxHeight / 2;
+  const markerHeight = markerBoxWidth / 2;
+  const markerWidth = markerBoxHeight / 2;
+  const arrowPoints = [
+    [0, 0],
+    [0, markerBoxHeight],
+    [markerBoxWidth, markerBoxHeight / 2],
+  ];
+
+  const defs = svg.append('defs');
+
+  defs.html(`
     <clipPath id="circle-cutout">
       <circle cx="${rad}" cy="${rad}" r=${rad} />
     </clipPath>
-  </defs>
     `);
 
-  svg.attr("width", width).attr("height", height);
+  defs
+    .append('marker')
+    .attr('id', 'arrow')
+    .attr('viewBox', [0, 0, markerBoxWidth, markerBoxHeight])
+    .attr('refX', refX)
+    .attr('refY', refY)
+    .attr('markerWidth', markerWidth)
+    .attr('markerHeight', markerHeight)
+    .attr('orient', 'auto-start-reverse')
+    .append('path')
+    .attr('d', d3.line()(arrowPoints))
+    .attr('stroke', 'black');
+
+  svg.attr('width', width).attr('height', height);
 
   const updateLocations = () => {
-    nodes.attr("transform", (node) => {
-      console.log("transform");
-      return `translate(${node.x},${node.y})`;
-    });
+    nodes.attr('transform', (node) => `translate(${node.x},${node.y})`);
 
     links = g_links
-      .selectAll("line")
-      .attr("x2", (edge) => edge.source.x)
-      .attr("y2", (edge) => edge.source.y)
-      .attr("x1", (edge) => edge.target.x)
-      .attr("y1", (edge) => edge.target.y);
+      .selectAll('line')
+      .attr('x1', (edge) => edge.source.x)
+      .attr('y1', (edge) => edge.source.y)
+      .attr('x2', (edge) => edge.target.x)
+      .attr('y2', (edge) => edge.target.y);
   };
 
   const simulation = d3
     .forceSimulation(nodesArray)
-    .force("charge", d3.forceManyBody().strength(charge))
+    .force('charge', d3.forceManyBody().strength(charge))
     .force(
-      "link",
+      'link',
       d3
         .forceLink(linksArray)
         .id((node) => node.id)
         .distance(dist)
-        .strength(1)
+        .strength(1),
     )
-    .force("x", d3.forceX(width / 2))
-    .force("y", d3.forceY(height / 2))
-    .on("tick", updateLocations);
+    .force('x', d3.forceX(width / 2))
+    .force('y', d3.forceY(height / 2))
+    .on('tick', updateLocations);
 
   const rect = svg
-    .append("rect")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", width)
-    .attr("height", height)
-    .attr("fill", "#ccc");
-  const g_links = svg.append("g").attr("class", "links");
-  const g_nodes = svg.append("g").attr("class", "nodes");
+    .append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', '#ccc');
+  const g_links = svg.append('g').attr('class', 'links');
+  const g_nodes = svg.append('g').attr('class', 'nodes');
 
-  let links = g_links
-    .selectAll("line")
-    .data(linksArray)
-    .enter()
-    .append("line")
-    .attr("x2", (edge) => edge.source.x)
-    .attr("y2", (edge) => edge.source.y)
-    .attr("x1", (edge) => edge.target.x)
-    .attr("y1", (edge) => edge.target.y);
+  let links;
+
+  const makeLinks = (someLinks) =>
+    someLinks
+      .enter()
+      .append('line')
+      .attr('marker-end', 'url(#arrow)')
+      .attr('x1', (edge) => edge.source.x)
+      .attr('y1', (edge) => edge.source.y)
+      .attr('x2', (edge) => edge.target.x)
+      .attr('y2', (edge) => edge.target.y);
 
   // Perform the next step in the replacement process.
   const replace = () => {
+    let editedNodes = [];
+    let renderMethod;
     if (currStep >= steps.length) {
       makeArrays();
       currStep = 0;
+      renderMethod = nuke;
     } else {
-      const { nodesDeleted, edgesAdded } = steps[currStep];
+      renderMethod = reRender;
+      const { nodesDeleted, edgesAdded, ...rest } = steps[currStep];
+      ({ editedNodes } = rest);
 
       nodesArray = nodesArray.filter((node) => !nodesDeleted.includes(node.id));
+
       prunedArray = linksArray.filter(
         (link) =>
           !(
             nodesDeleted.includes(link.source.id) ||
             nodesDeleted.includes(link.target.id)
-          )
+          ),
       );
       linksArray = [
         ...prunedArray,
@@ -115,62 +149,116 @@ window.onload = async () => {
       currStep++;
     }
 
+    console.log(nodesArray, nodes.data(), editedNodes);
+
     simulation.nodes(nodesArray);
 
     simulation.force(
-      "link",
+      'link',
       d3
         .forceLink(linksArray)
         .id((node) => node.id)
         .distance(dist)
-        .strength(1)
+        .strength(1),
     );
     simulation.alpha(1);
     simulation.restart();
-    reRender();
+    renderMethod();
+
+    nodes
+      .select('.preview')
+      .filter(
+        (node) =>
+          editedNodes.findIndex((editRecord) => editRecord.id == node.id) !==
+          -1,
+      )
+      .html((node) => {
+        console.log('modifying', node.id);
+        const editRecord = editedNodes.find(
+          (editRecord) => editRecord.id == node.id,
+        );
+        return editRecord.svg;
+      })
+      .selectAll('svg')
+      .attr('width', rad * 2)
+      .attr('height', rad * 2);
+
+    nodes
+      .select('circle')
+      .filter(
+        (node) =>
+          editedNodes.findIndex((editRecord) => editRecord.id == node.id) !==
+          -1,
+      )
+      .attr('r', (node) => {
+        const editRecord = editedNodes.find(
+          (editRecord) => editRecord.id == node.id,
+        );
+        return getRadius(editRecord);
+      });
   };
 
   const makeNodes = (someNodes) => {
     const nodes = someNodes
       .enter()
-      .append("g")
-      .attr("class", "circle")
-      .attr("transform", (node) => `translate(${node.x},${node.y})`)
-      .on("click", replace);
+      .append('g')
+      .attr('class', 'circle')
+      .attr('transform', (node) => `translate(${node.x},${node.y})`)
+      .on('click', replace);
+
+    nodes.append('circle').attr('cx', 0).attr('cy', 0).attr('r', getRadius);
 
     nodes
-      .append("circle")
-      .attr("cx", 0)
-      .attr("cy", 0)
-      .attr("r", (node) => rad + 2);
-
-    nodes
-      .append("g")
-      .attr("transform", `translate(${-rad},${-rad})`)
-      .attr("clip-path", "url(#circle-cutout)")
+      .append('g')
+      .attr('class', 'preview')
+      .attr('transform', `translate(${-rad},${-rad})`)
+      .attr('clip-path', 'url(#circle-cutout)')
       .html((node) => node.svg)
-      .selectAll("svg")
-      .attr("width", rad * 2)
-      .attr("height", rad * 2);
+      .selectAll('svg')
+      .attr('width', rad * 2)
+      .attr('height', rad * 2);
 
     return nodes;
   };
 
-  let nodes = makeNodes(g_nodes
-    .selectAll("g")
-    .data(nodesArray)
-  )
-  
+  rect.on('click', replace, true);
 
-  rect.on("click", replace, true);
+  const nuke = () => {
+    const remove_nodes = g_nodes.selectAll('g.circle').data([]);
+    remove_nodes.exit().remove();
+
+    nodes = makeNodes(g_nodes.selectAll('g.circle').data(nodesArray));
+
+    links = makeLinks(g_links.selectAll('line').data(linksArray));
+  };
+
+  nuke();
 
   const reRender = () => {
-    const update_nodes = g_nodes.selectAll("g.circle").data(nodesArray);
+    // I need to explicitly split my "zombie" DOM nodes into two groups: one which matches one-to-one, in the right order, with my data nodes; and another which has all of my DOM nodes to kill.
+
+    const update_nodes = g_nodes
+      .selectAll('g.circle')
+      .filter(
+        (node) =>
+          nodesArray.findIndex((arrNode) => arrNode.id == node.id) !== -1,
+      )
+      .data(nodesArray);
     update_nodes.exit().remove();
+
+    const remove_nodes = g_nodes
+      .selectAll('g.circle')
+      .filter(
+        (node) =>
+          nodesArray.findIndex((arrNode) => arrNode.id == node.id) === -1,
+      )
+      .data([]);
+    remove_nodes.exit().remove();
+
     nodes = makeNodes(update_nodes).merge(update_nodes);
 
-    const update_links = g_links.selectAll("line").data(linksArray);
+    const update_links = g_links.selectAll('line').data(linksArray);
     update_links.exit().remove();
-    links = update_links.enter().append("line").merge(update_links);
+    links = makeLinks(update_links).merge(update_links);
   };
 };
